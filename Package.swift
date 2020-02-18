229c007379f67d04591b4533a56b0b6dff87a1ea
twodayslate <zac@gorak.us>
2020-02-17 21:57:29 -0500

    Using version instead of master

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["AddressURL"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/evanrobertson/EmailValidator.git", .branch("master"))
+        .package(url: "https://github.com/evanrobertson/EmailValidator.git", from: "1.0.3")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
